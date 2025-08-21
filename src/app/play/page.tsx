cb76a3089b284bfb108cd680292cddbe0364d721
huangsj475 <2679237343@qq.com>
2025-08-22 01:14:12 +0800

    播放器标题显示优化

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1302,7 +1302,7 @@ useEffect(() => {
       artPlayerRef.current = new Artplayer({
         container: artRef.current,
         url: videoUrl,
-        title: '',
+        title: null,
         poster: videoCover,
         volume: 0.7,
         isLive: false,
