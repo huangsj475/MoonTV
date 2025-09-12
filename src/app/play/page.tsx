ec1c18ced317000052c138e9ab857e7fee804cae
huangsj475 <2679237343@qq.com>
2025-09-12 22:48:16 +0800

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1337,8 +1337,7 @@ if (oldTitleLayer) {
       // 销毁播放器实例
       artPlayerRef.current.destroy();
       artPlayerRef.current = null;
-      // 清理 window 上的引用
-    delete (window as any).artPlayer;
+    
     }
 
     try {
@@ -1504,8 +1503,7 @@ if (oldTitleLayer) {
                   }
                   artPlayerRef.current.destroy();
                   artPlayerRef.current = null;
-                      // 清理 window 上的引用
-                  delete (window as any).artPlayer;
+                     
                 }
                 setBlockAdEnabled(newVal);
               } catch (_) {
@@ -1596,9 +1594,7 @@ if (oldTitleLayer) {
           },
         ],
       });
-      //--------- 挂载到 window---------
-(window as any).artPlayer = artPlayerRef.current; 
-
+      
        // -----新增控制栏绑定事件监听器------
               // 控制栏隐藏时隐藏标题
         artPlayerRef.current.on('controlshide',  () => {
