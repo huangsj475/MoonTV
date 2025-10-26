c1a754ee4a3667fec0cdd9a1ebe20d487db0884d
huangsj475 <2679237343@qq.com>
2025-10-26 14:40:59 +0800

    Update downstream.ts

--- src/lib/downstream.ts
+++ src/lib/downstream.ts
@@ -55,7 +55,7 @@ export async function searchFromApi(
 
       // 使用正则表达式从 vod_play_url 提取 m3u8 链接
       if (item.vod_play_url) {
-        const m3u8Regex = /([^\$\n]+)\$(https?:\/\/[^"'\s]+?\.m3u8)/g;
+        const m3u8Regex = /([^$\n]+)\$(https?:\/\/[^"'\s]+?\.m3u8)/g;
         // 先用 $$$ 分割
         const vod_play_url_array = item.vod_play_url.split('$$$');
         // 对每个分片做匹配，取匹配到最多的作为结果
