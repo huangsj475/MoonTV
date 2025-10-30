24c909fa97d2442a397535486438531a814db0bb
huangsj475 <2679237343@qq.com>
2025-10-30 21:00:52 +0800

    Update page.tsx

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -17,6 +17,7 @@ import { SearchResult } from '@/lib/types';
 import { yellowWords } from '@/lib/yellow';
 
 import PageLayout from '@/components/PageLayout';
+import SearchSuggestions from '@/components/SearchSuggestions';//新增搜索建议
 import VideoCard from '@/components/VideoCard';
 
 function SearchPageClient() {
