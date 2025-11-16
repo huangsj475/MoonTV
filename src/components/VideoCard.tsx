7e524ef5826f975d353d8383aecb99fe2b1d4457
huangsj475 <2679237343@qq.com>
2025-11-16 17:43:21 +0800

    Update VideoCard.tsx

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -136,7 +136,7 @@ export default function VideoCard({
 		}
 	  }else if (favorited !== null) {
 		// 如果已经知道状态，只显示提示，不重复检查
-		setTooltip(favorited ? '已收藏' : '未收藏');
+		setTooltip(favorited ? '✅已收藏' : '❌未收藏');
 	  }
 	}, [favorited, actualSource, actualId, checkingFavorite, from]);
 	//----改动：鼠标悬停，划过获取收藏状态--------
