8cb40a4b6250adc5236e6d8b16e533f4081c886f
huangsj475 <2679237343@qq.com>
2025-12-10 22:23:19 +0800

    Update page.tsx

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -4,7 +4,7 @@
 import { ChevronUp, Search, X } from 'lucide-react';
 import { useRouter, useSearchParams } from 'next/navigation';
 //import { Suspense, useEffect, useMemo, useState } from 'react';
-import React, { Suspense, useEffect, useMemo, useState } from 'react';
+import React, { startTransition, Suspense, useEffect, useMemo, useRef, useState } from 'react';
 
 import {
   addSearchHistory,
