26353d20e46e3da84eb60ae3550390f58031520e
huangsj475 <2679237343@qq.com>
2025-12-15 22:08:23 +0800

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -2112,14 +2112,14 @@ useEffect(() => {
 	          // 请求显示，且当前是隐藏状态 → 显示
 	          timeElement.style.display = 'block';
 	          titleElement.style.display = 'block';
-	          isControlBarVisible = false;
+	          isControlBarVisible = true;
 	          console.log('显示控制栏');
 	          
 	        } else if (!show && isControlBarVisible) {
 	          // 请求隐藏，且当前是显示状态 → 隐藏
 	          timeElement.style.display = 'none';
 	          titleElement.style.display = 'none';
-	          isControlBarVisible = true;
+	          isControlBarVisible = false;
 	          console.log('隐藏控制栏');
 	        }
         }
