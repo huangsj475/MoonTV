f6aba66311d07e2b2debc4b973216da8ae4f8ecb
huangsj475 <2679237343@qq.com>
2025-12-21 05:41:13 +0800

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1590,13 +1590,13 @@ useEffect(() => {
 			  
 			 /*  ---在BUFFER_APPENDED缓冲时，恢复进度，
 				导致hls空洞错误（即已经缓冲一部分hls流，恢复进度到未缓冲的地方，
-				hls一开始就清空旧的缓冲，开始新缓冲，所以在BUFFER_CREATED缓冲创建时跳转
+				hls一开始就清空旧的缓冲，开始新缓冲，所以在BUFFER_APPENDING缓冲时跳转
 			 */
-			  //视频缓冲创建
-			hls.on(Hls.Events.BUFFER_CREATED, (event, data) => {
-				if(!videoReadyRef.current){
+			  //视频开始缓冲
+			hls.on(Hls.Events.BUFFER_APPENDING, (event, data) => {
+				
 					videoReadyRef.current = true;
-				}
+				
 			});
 			  //数据进入缓冲区
 			/*hls.on(Hls.Events.BUFFER_APPENDED, (event, data) => {
