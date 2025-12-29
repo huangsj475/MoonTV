3ed369f1bf4d94ea2685b1f74d8f00b5a53f9e17
huangsj475 <2679237343@qq.com>
2025-12-29 16:43:23 +0800

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1815,11 +1815,11 @@ useEffect(() => {
               lowLatencyMode: true, // 开启低延迟 LL-HLS
 
               /* 缓冲/内存相关 */
-              maxBufferLength: 40, // 向前缓存=这个值-backBufferLength，过大容易导致高延迟
-              backBufferLength: 10, // 仅保留 20s 已播放内容，避免内存占用
+              maxBufferLength: 50, // 向前缓存=这个值-backBufferLength，过大容易导致高延迟
+              backBufferLength: 20, // 仅保留 20s 已播放内容，避免内存占用
               maxBufferSize: 60 * 1000 * 1000, // 约 60MB，超出后触发清理
 
-			  maxMaxBufferLength: 60,//绝对的最大允许缓冲区长度
+			  maxMaxBufferLength: 70,//绝对的最大允许缓冲区长度
 
               /* 自定义loader */
               loader: blockAdEnabledRef.current
